2c5072a0c988965da16c1285b05c0d4d5f677589
Carlos Valle <carlos.momos@gmail.com>
2018-07-05 16:37:50 -0600

    Classrooms Fragment is ready for deployment

--- app/src/main/java/me/djangosolutions/kenary/Fragments/FragClass.kt
+++ app/src/main/java/me/djangosolutions/kenary/Fragments/FragClass.kt
@@ -17,27 +17,29 @@ import me.djangosolutions.kenary.R
 class FragClass : Fragment() {
     private var v: View? = null
     private var recyclerView: RecyclerView? = null
+
     var list: List<ModelClass>? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         v = inflater.inflate(R.layout.classrooms, container, false)
-        recyclerView = v!!.findViewById(R.id.classroomrv)
 
-        val linearLayoutManager = LinearLayoutManager(context)
-        recyclerView!!.layoutManager = linearLayoutManager
-        val adapter = ClassRvAdapter(context, getList())
-        recyclerView!!.adapter = adapter
+
+
 
         return v
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        recyclerView = v!!.findViewById(R.id.classroomrv)
+        val linearLayoutManager = LinearLayoutManager(context)
+        recyclerView!!.layoutManager = linearLayoutManager
+        val adapter = ClassRvAdapter(view.context, getList())
+        recyclerView!!.adapter = adapter
     }
 
-    fun getList(): List<ModelClass> {
+    fun getList(): ArrayList<ModelClass> {
         //Aqui deberia ir la funcion que obtendra la lista de class rooms
         val list = ArrayList<ModelClass>()
         list.add(ModelClass("1", "7 Classrooms", "Fluídos"))
