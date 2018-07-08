17bca31081bffae405ffa35fe6cfa68e69ac07bc
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-07-08 16:59:54 -0500

    changed bio to institution field

--- app/src/main/java/me/djangosolutions/kenary/Firebase/Model/UserM.kt
+++ app/src/main/java/me/djangosolutions/kenary/Firebase/Model/UserM.kt
@@ -1,7 +1,7 @@
 package me.djangosolutions.kenary.Firebase.Model
 
 data class UserM(val diplayName:String,
-                 val bio:String?,
+                 val institution:String?,
                  val email:String,
                  val profilePicPath:String?){
     constructor():this("","","",null)
