e0d4cb332ef79a2a12dc8cad3ffd283af2879e51
Nelson Castro Dev <00043516@uca.edu.sv>
2018-07-08 19:19:28 -0600

    Optimizing code

--- app/src/main/java/me/djangosolutions/kenary/Adapters/ViewPagerAdapter.kt
+++ app/src/main/java/me/djangosolutions/kenary/Adapters/ViewPagerAdapter.kt
@@ -5,18 +5,16 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.PagerAdapter
-import me.djangosolutions.kenary.Fragments.FragClass
-import me.djangosolutions.kenary.Fragments.FragTuto
+import me.djangosolutions.kenary.Fragments.PFragHomeContentTab
 import me.djangosolutions.kenary.R
-import java.util.ArrayList
 
 class ViewPagerAdapter(private val context: Context, fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
 
     override fun getItem(position: Int): Fragment = when (position) {
-        0 -> FragClass()
-        1 -> FragTuto()
-        else -> FragClass()
+        0 -> PFragHomeContentTab.newInstance("classroom")
+        1 -> PFragHomeContentTab.newInstance("tutorias")
+        else -> PFragHomeContentTab()
     }
 
     override fun getCount(): Int =  2
