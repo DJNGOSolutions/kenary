ed148c574689d304e6ebe90106e109fc75d66c23
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-07-09 23:19:50 -0500

    Added message fragments

--- app/src/main/java/me/djangosolutions/kenary/Fragments/PFragMens.kt
+++ app/src/main/java/me/djangosolutions/kenary/Fragments/PFragMens.kt
@@ -1,21 +1,80 @@
 package me.djangosolutions.kenary.Fragments
 
+
+
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.firestore.ListenerRegistration
 
-class PFragMens: Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.OnItemClickListener
+import com.xwray.groupie.Section
+import com.xwray.groupie.kotlinandroidextensions.Item
+import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import kotlinx.android.synthetic.main.fragment_people.*
+import me.djangosolutions.kenary.ChatActivity
+import me.djangosolutions.kenary.Firebase.Model.RecyclerView.item.PersonItem
+import me.djangosolutions.kenary.Firebase.Model.Utils.FirebaseUtil
+import me.djangosolutions.kenary.R
+import org.jetbrains.anko.support.v4.startActivity
+
+
+class PFragMens : Fragment() {
+
+    private lateinit var userListenerRegistration: ListenerRegistration
+
+    private var shouldInitRecyclerView = true
+
+    private lateinit var peopleSection: Section
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        userListenerRegistration =
+                FirebaseUtil.addUsersListener(this.activity!!, this::updateRecyclerView)
+
+        return inflater.inflate(R.layout.fragment_people, container, false)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        FirebaseUtil.removeListener(userListenerRegistration)
+        shouldInitRecyclerView = true
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun updateRecyclerView(items: List<Item>) {
+        fun init() {
+            recycler_view_people.apply {
+                layoutManager = LinearLayoutManager(this@PFragMens.context)
+                adapter = GroupAdapter<ViewHolder>().apply {
+                    peopleSection = Section(items)
+                    add(peopleSection)
+                    setOnItemClickListener(onItemClick)
+                }
+            }
+            shouldInitRecyclerView = false
+        }
+
+        fun updateItems() = peopleSection.update(items)
+
+        if (shouldInitRecyclerView)
+            init()
+        else
+            updateItems()
+
     }
+
+    private val onItemClick = OnItemClickListener { item, view ->
+        if (item is PersonItem) {
+            startActivity<ChatActivity>(
+                    //AppConstants.USER_NAME to item.person.name,
+                    //AppConstants.USER_ID to item.userId
+            )
+        }
+    }
+
 }
