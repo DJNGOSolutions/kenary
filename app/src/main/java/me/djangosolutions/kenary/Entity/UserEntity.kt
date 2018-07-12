c95c5e3aa9af6e342738bb24ec52a36a7accaecb
Marcelo Martínez <marcelo2m1097@gmail.com>
2018-07-12 03:14:14 -0600

    Entities created, again

---
+++ app/src/main/java/me/djangosolutions/kenary/Entity/UserEntity.kt
@@ -0,0 +1,19 @@
+package me.djangosolutions.kenary.Entity
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+/**
+ * Created by Marcelo on 12/07/2018.
+ */
+@Entity(tableName = "User")
+data class UserEntity(@PrimaryKey @ColumnInfo(name = "idUser") var IdUser: Int = 0,
+                @ColumnInfo(name = "idGoogle") var IdGoogle: Int = 0,
+                @ColumnInfo(name = "idTipo") var IdTipo: Int = 0,
+                @ColumnInfo(name = "userPhoto") var UserPhoto: String = "",
+                @ColumnInfo(name = "userName") var UserName: String = "",
+                @ColumnInfo(name = "name") var Name: String = "",
+                @ColumnInfo(name = "userInstitution") var UserInstitution: String = "",
+                @ColumnInfo(name = "userPassword") var UserPassword: String = "") {
+}
