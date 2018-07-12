340fea41e8ad2743e0d41201e4e80b613163c508
Marcelo Martínez <marcelo2m1097@gmail.com>
2018-07-12 03:46:00 -0600

    Creation of DAO's, again

---
+++ app/src/main/java/me/djangosolutions/kenary/Database/Daos/CategoryDao.kt
@@ -0,0 +1,26 @@
+package me.djangosolutions.kenary.Database.Daos
+
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import me.djangosolutions.kenary.Entity.CategoryEntity
+
+/**
+ * Created by Marcelo on 12/07/2018.
+ */
+@Dao
+interface CategoryDao {
+    @Query("SELECT * FROM category_table")
+    fun getAllCategories(): LiveData<List<CategoryEntity>>
+
+    @Query("SELECT * FROM category_table WHERE idCategory = :IdCategory")
+    fun getCategoryById(): LiveData<CategoryEntity>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(categoryEntity: CategoryEntity)
+
+    @Query("DELETE FROM category_table")
+    fun deleteAll()
+}
--- app/src/main/java/me/djangosolutions/kenary/Entity/TutorialEntity.kt
+++ app/src/main/java/me/djangosolutions/kenary/Entity/TutorialEntity.kt
@@ -7,7 +7,7 @@ import android.arch.persistence.room.PrimaryKey
 /**
  * Created by Marcelo on 12/07/2018.
  */
-@Entity(tableName = "Tutorial")
+@Entity(tableName = "tutorial_table")
 data class TutorialEntity (@PrimaryKey @ColumnInfo(name = "idTutorial") var IdTutorial: Int = 0,
                            @ColumnInfo(name = "categoryName") var CategoryName: String = "",
                            @ColumnInfo(name = "subjectName") var SubjectName: String = "",
